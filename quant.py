c38dbe9b593570bfa54df67e8a3a851543cf5325
kwan <kanldark135@gmail.com>
2023-08-23 19:18:14 +0900

    v. 230823

--- quant.py
+++ quant.py
@@ -17,7 +17,8 @@ def sort(df):
                 df = df
             else:
                 raise KeyError("Adj Close / Close / Last_Price 형태의 종가가 dataframe 에 존재하도록 하거나 그냥 종가 한줄 넣으세요")
-
+    
+    df.columns = ['close']
     return df
 
 def df_cumret(df):
@@ -67,32 +68,16 @@ class ta:
         
         # ohlc 던 close only던 df_price feed 하면 종가만 튀어나오게 수정
 
-        try:
-            self.df = df['Adj Close']
-        except KeyError:
-            try:
-                potential_column_name = ['Close', "Price", "Last_Price"]
-                self.df = df[df.columns.isin(potential_column_name)]
-
-            except KeyError:
-
-                if len(df.columns) == 1:
-                    self.df = df
-                else:
-                    raise KeyError("Adj Close / Close / Last_Price 형태의 종가가 dataframe 에 존재하도록 하거나 그냥 종가 한줄 넣으세요")
-        
-        df.columns = ['close']
-
+        self.df = sort(df)
         self.ret = self.df.pct_change(1).iloc[1:]
         self.cumret = (self.ret + 1).cumprod()
-
 
     def bollinger_band(self, days = 20, width = 2):
 
         df = self.df.to_frame()
         stdev = df.rolling(days).std()
 
-        df = df.assign(center = df.rolling(days).mean(), bb_upper = df.rolling(days).mean().add(width * stdev), bb_lower = df.rolling(days).mean().sub(width * stdev))
+        df = df.assign(bb_center = df.rolling(days).mean(), bb_upper = df.rolling(days).mean().add(width * stdev), bb_lower = df.rolling(days).mean().sub(width * stdev))
 
         return df
 
