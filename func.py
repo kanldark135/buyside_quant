9a8c5f662a11fb570d5b6106e0891f1ff395215d
kwan <kanldark135@gmail.com>
2023-09-05 14:43:32 +0900

    v 230905

--- func.py
+++ func.py
@@ -3,8 +3,7 @@ import numpy as np
 from openbb_terminal.sdk import openbb
 import quant
 
-
-def load_stocks(tickers : list, start_date = "1909-01-01", end_date = "2099-01-01"):
+def load_stocks(tickers : list, drop_na_date = True, start_date = "1909-01-01", end_date = "2099-01-01"):
 
     start_date = pd.to_datetime(start_date)
     end_date = pd.to_datetime(end_date)
@@ -16,8 +15,12 @@ def load_stocks(tickers : list, start_date = "1909-01-01", end_date = "2099-01-0
         df_raw = openbb.stocks.load(i, start_date = start_date)
         temp = quant.close_only(df_raw, column_rename = i)
         df = pd.concat([df, temp], axis = 1)
-        df = df.dropna()
-    
+
+        if drop_na_date:
+            df = df.dropna()
+        else:
+            df = df
+
     return df
 
 def extract_last_price(df, interval = "M"):
@@ -28,11 +31,12 @@ def extract_last_price(df, interval = "M"):
     grouped = df.resample(interval)
     
     # 2. last day 뽑아내는 함수 및 적용
-    def extract_last_day(grouped):
-        if not grouped.empty:
-            return grouped.iloc[[-1]].index
-
-        # downsampling 하게되면 index[-1] 사용
+    def extract_last_day(one_group):
+        try:
+            res = one_group.iloc[[-1]].index
+        except:
+            res = np.nan
+        return res
     
     # 3. 원래 가격 df 에서 마지막 날만 reindex로 filtering. df.filter(items = last_days, axis = 0 도 동일)
     last_days = grouped.apply(extract_last_day).dropna().values
